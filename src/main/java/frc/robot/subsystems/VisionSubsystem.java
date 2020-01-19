935016c108b4b4ce728767826e06c8e04ad42ec1
Dan Dumitrescu <dan.dumitrescu@live.com>
2020-01-18 16:27:03 -0800

    Added the data from limelight camera to smartdashboard

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -7,25 +7,68 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class VisionSubsystem extends SubsystemBase {
   /**
    * Creates a new VisionSubsystem.
    */
-  public VisionSubsystem() {
+
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry tv = table.getEntry("tv");
+
+  public static boolean targetAquired = false;
+  public static double horizontalOffset = 0.0f;
+
+  public VisionSubsystem() {    
+
+  }
+
+  public void pollCamera() {
+
+    targetAquired = tv.getBoolean(false);
+    horizontalOffset = tx.getDouble(0.0);
+
+
+    SmartDashboard.putBoolean("Limelight/TargetAquired", targetAquired);
+    SmartDashboard.putNumber("Limelight/HorizontalOffset", horizontalOffset);
+
+
+       
+  }
+  public static void getInRange()  {
+
+    /*
+    float kpDistance = -0.1f;
+    float currentDistance = Estimate_Distance();
+
+    float distanceError = desiredDistance - currentDistance;
+    drivingAdjust = kpDistance + distanceError;
+
+    leftCommand += distanceAdjust;
+    rightCommand += distanceAdjust;
+    */
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    /*
     if(Robot.m_oi.Abutton()) {
 
     }
+    */
 
-  }
+    pollCamera();
 
-  
+  } 
+
 }
