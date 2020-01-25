4c923aa8fe9ac635f4e1207d43f81831fac21158
PhillipVolkov <38336223+PhillipVolkov@users.noreply.github.com>
2020-01-25 12:49:38 -0800

    update for new sparks

--- src/main/java/frc/robot/subsystems/TestSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestSubsystem.java
@@ -6,6 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.commands.TestCommand;
@@ -13,6 +14,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.revrobotics.CANError;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -22,16 +25,42 @@ import frc.robot.Robot;
 
 public class TestSubsystem extends Subsystem {
   CANSparkMax rightFrontTalon = new CANSparkMax(RobotMap.rightFrontTalon, MotorType.kBrushless);
-  WPI_TalonSRX rightRearTalon = new WPI_TalonSRX(RobotMap.rightRearTalon);
+  CANSparkMax rightRearTalon = new CANSparkMax(RobotMap.rightRearTalon, MotorType.kBrushless);
   CANSparkMax leftFrontTalon = new CANSparkMax(RobotMap.leftFrontTalon, MotorType.kBrushless);
-  WPI_TalonSRX leftRearTalon = new WPI_TalonSRX(RobotMap.leftRearTalon);
-  DifferentialDrive sparkDrive = new DifferentialDrive(leftFrontTalon,rightFrontTalon);
-  DifferentialDrive talonDrive = new DifferentialDrive(leftRearTalon,rightRearTalon);
+  CANSparkMax leftRearTalon = new CANSparkMax(RobotMap.leftRearTalon, MotorType.kBrushless);
+  SpeedControllerGroup sparkDriveLeft = new SpeedControllerGroup(leftFrontTalon,leftRearTalon);
+  SpeedControllerGroup sparkDriveRight = new SpeedControllerGroup(rightFrontTalon,rightRearTalon);
+  DifferentialDrive sparkDrive = new DifferentialDrive(sparkDriveLeft,sparkDriveRight);
   XboxController driveControl = new XboxController(0);
 
+  public TestSubsystem() {
+    rightFrontTalon.setSmartCurrentLimit(60);
+    rightRearTalon.setSmartCurrentLimit(60);
+    leftFrontTalon.setSmartCurrentLimit(60);
+    leftRearTalon.setSmartCurrentLimit(60);
+
+    rightFrontTalon.setClosedLoopRampRate(5);
+    rightRearTalon.setClosedLoopRampRate(5);
+    leftFrontTalon.setClosedLoopRampRate(5);
+    leftRearTalon.setClosedLoopRampRate(5);
+    // leftFrontTalon.setInverted(false);
+    // rightFrontTalon.setInverted(true);
+    // leftRearTalon.setInverted(false);
+    // rightRearTalon.setInverted(true);
+  }
+
   public void arcadeDrive(double forward, double turn) {
-    sparkDrive.arcadeDrive(-forward, turn);
-    talonDrive.arcadeDrive(forward, turn);
+    forward*=0.8;
+    turn*=0.8;
+    
+    sparkDrive.arcadeDrive(forward, -turn);
+  }
+
+  public void execute() {
+    // SmartDashboard.putNumber("PID/leftEncoder", rightFrontTalon.getEncoder().getVelocity());
+    // SmartDashboard.putNumber("PID/rightEncoder", rightRearTalon.getEncoder().getVelocity());
+    // SmartDashboard.putNumber("PID/leftSpeed", leftFrontTalon.getEncoder().getVelocity());
+    // SmartDashboard.putNumber("PID/rightSpeed", leftRearTalon.getEncoder().getVelocity());
   }
 
   public void initDefaultCommand() {
