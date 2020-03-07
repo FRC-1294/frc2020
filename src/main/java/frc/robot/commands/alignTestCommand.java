211ecc1108f053f8d6322c9d2284d7f74262876a
k-he <s-kehe@lwsd.org>
2020-03-06 20:55:14 -0800

    made beginning of alignTestCommand

---
+++ src/main/java/frc/robot/commands/alignTestCommand.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveAutoSubsystem;
+import frc.robot.subsystems.TwentyThreeStabWounds;
+
+//used to test aligning
+public class alignTestCommand extends CommandBase {
+  /**
+   * Creates a new alignTestCommand.
+   */
+
+   DriveAutoSubsystem    driveAuto;
+   TwentyThreeStabWounds vision;
+   DictatorLocator alignToTarget;
+  public alignTestCommand(DriveAutoSubsystem driveAuto, TwentyThreeStabWounds vision) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.driveAuto = driveAuto;
+    this.vision    = vision;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(!alignToTarget.isScheduled()){
+      alignToTarget = new DictatorLocator(vision, driveAuto);
+      alignToTarget.schedule();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    // return false;
+    return alignToTarget.isFinished();
+  }
+}
